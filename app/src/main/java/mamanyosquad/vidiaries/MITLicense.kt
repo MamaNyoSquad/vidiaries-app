acc15a98a50b104529b0756cfcb7b39b8c346db6
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-03-07 22:51:52 +0800

    0.2.3-prerelease
    in this update:
    - MIT license screen
    - updated Main Story button (to FAB)
    - Japanese translation
    -

---
+++ app/src/main/java/mamanyosquad/vidiaries/MITLicense.kt
@@ -0,0 +1,13 @@
+package mamanyosquad.vidiaries
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MITLicense : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mitlicense)
+    }
+
+}
--- app/src/main/java/mamanyosquad/vidiaries/MainActivity.kt
+++ app/src/main/java/mamanyosquad/vidiaries/MainActivity.kt
@@ -2,10 +2,11 @@ package mamanyosquad.vidiaries
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +14,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val redirMainStory: Button = findViewById(R.id.btnMainStoryRedirect)
-        redirMainStory.setOnClickListener {
+        val viMainStory: FloatingActionButton = findViewById(R.id.btnMainStory)
+        viMainStory.setOnClickListener {
             val vi = Intent(Intent.ACTION_VIEW, Uri.parse("https://bit.ly./VIDiariesRealTime"))
             startActivity(vi)
         }
+
+        val btnMITLicense: Button = findViewById(R.id.btnMITLicense)
+        btnMITLicense.setOnClickListener {
+            val mit = Intent(this@MainActivity, MITLicense::class.java)
+            startActivity(mit)
+        }
+
+        val btnInsideStory1: Button = findViewById(R.id.btnInsideStory1)
+        btnInsideStory1.setOnClickListener {
+            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
+        }
+        val btnInsideStory2: Button = findViewById(R.id.btnInsideStory2)
+        btnInsideStory2.setOnClickListener {
+            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
+        }
+        val btnInsideStory3: Button = findViewById(R.id.btnInsideStory3)
+        btnInsideStory3.setOnClickListener {
+            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
+        }
+        val btnInsideStory4: Button = findViewById(R.id.btnInsideStory4)
+        btnInsideStory4.setOnClickListener {
+            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
+        }
+        val btnInsideStory5: Button = findViewById(R.id.btnInsideStory5)
+        btnInsideStory5.setOnClickListener {
+            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
     }
 
 }
