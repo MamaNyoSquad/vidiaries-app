f43ff0b8f4586b0e02e7819d18a816a82b7304dc
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-03-08 20:45:07 +0800

    mamanyosquad.vidiaries_0.2.5-prerelease
    in this update:
    - added splash screens
    - keystore added

--- app/src/main/java/mamanyosquad/vidiaries/MainActivity.kt
+++ app/src/main/java/mamanyosquad/vidiaries/MainActivity.kt
@@ -1,12 +1,9 @@
 package mamanyosquad.vidiaries
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,42 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viMainStory: FloatingActionButton = findViewById(R.id.btnMainStory)
-        viMainStory.setOnClickListener {
-            val vi = Intent(Intent.ACTION_VIEW, Uri.parse("https://bit.ly./VIDiariesRealTime"))
-            startActivity(vi)
-        }
-
-        val btnMITLicense: Button = findViewById(R.id.btnMITLicense)
-        btnMITLicense.setOnClickListener {
-            val mit = Intent(this@MainActivity, MITLicense::class.java)
-            startActivity(mit)
-        }
-
-        val btnInsideStory1: Button = findViewById(R.id.btnInsideStory1)
-        btnInsideStory1.setOnClickListener {
-            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
-        }
-        val btnInsideStory2: Button = findViewById(R.id.btnInsideStory2)
-        btnInsideStory2.setOnClickListener {
-            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
-        }
-        val btnInsideStory3: Button = findViewById(R.id.btnInsideStory3)
-        btnInsideStory3.setOnClickListener {
-            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
-        }
-        val btnInsideStory4: Button = findViewById(R.id.btnInsideStory4)
-        btnInsideStory4.setOnClickListener {
-            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
-        }
-        val btnInsideStory5: Button = findViewById(R.id.btnInsideStory5)
-        btnInsideStory5.setOnClickListener {
-            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
-        }
-
-
-
-
+        Handler().postDelayed({
+            val i = Intent(
+                this@MainActivity,
+                homescreen::class.java
+            )
+            startActivity(i)
+            finish()
+        }, 4500)
     }
 
 }
