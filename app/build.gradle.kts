1d9b89d52040dd563f2d9d41f71ba78076900944
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-03-16 21:26:30 +0800

    mamanyosquad.vidiaries_v0.3.0-prerelease
    this UNRELEASED update optimizes the splash screen for Android 12+ implementation, as well as other adjustments/implementations of code and theming options to fix some bugs.
    
    this will be included in a later revision update pre-release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "mamanyosquad.vidiaries"
         minSdk = 21
         targetSdk = 33
-        versionCode = 7
-        versionName = "0.3.0"
+        versionCode = 8
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         versionNameSuffix = "-prerelease"
@@ -48,4 +48,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.core:core-splashscreen:1.0.0")
 }
--- app/src/main/java/mamanyosquad/vidiaries/MainActivity.kt
+++ app/src/main/java/mamanyosquad/vidiaries/MainActivity.kt
@@ -1,24 +1,59 @@
 package mamanyosquad.vidiaries
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
-        Handler().postDelayed({
-            val i = Intent(
-                this@MainActivity,
-                HomeScreen::class.java
-            )
-            startActivity(i)
-            finish()
-        }, 4500)
+        val viMainStory: FloatingActionButton = findViewById(R.id.btnMainStory)
+        viMainStory.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://bit.ly./VIDiariesRealTime")))
+        }
+
+        val btnMITLicense: Button = findViewById(R.id.btnMITLicense)
+        btnMITLicense.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MITLicense::class.java))
+        }
+
+        fun notImplemented() {
+            Toast.makeText(applicationContext, "Not implemented yet", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnInsideStory1: Button = findViewById(R.id.btnInsideStory1)
+        btnInsideStory1.setOnClickListener {
+            notImplemented()
+        }
+        val btnInsideStory2: Button = findViewById(R.id.btnInsideStory2)
+        btnInsideStory2.setOnClickListener {
+            notImplemented()
+        }
+        val btnInsideStory3: Button = findViewById(R.id.btnInsideStory3)
+        btnInsideStory3.setOnClickListener {
+            notImplemented()
+        }
+        val btnInsideStory4: Button = findViewById(R.id.btnInsideStory4)
+        btnInsideStory4.setOnClickListener {
+            notImplemented()
+        }
+        val btnInsideStory5: Button = findViewById(R.id.btnInsideStory5)
+        btnInsideStory5.setOnClickListener {
+            notImplemented()
+        }
+        val btnInsideStory6: Button = findViewById(R.id.btnInsideStory6)
+        btnInsideStory6.setOnClickListener {
+            notImplemented()
+        }
     }
 
 }
